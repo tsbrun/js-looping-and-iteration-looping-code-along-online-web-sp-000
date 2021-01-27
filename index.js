5cc106e6b03bbb6a6a682859e0e7b5149b36b4ec
TSephanya Brun <tsbrun@icloud.com>
2021-01-27 15:48:26 -0500

    commit tree 050e1db2b4210f159bd8d6711f10950b0979b0fa

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards (names, event) {
+    let cards = [];
+
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return cards;
+}
+
+function countDown(int) {
+    while (int > -1) {
+        console.log(int);
+        int--;
+    }
+}
